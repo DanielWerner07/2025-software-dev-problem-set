c29443931272b85e39f46b289dfec49bca696632
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2025-01-06 17:46:04 -0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -19,3 +19,15 @@
 # Constraints:
 #     1 <= nums.length <= 105
 #     -109 <= nums[i] <= 109
+
+def containsDup (nums):
+    inArray = set()
+    for num in nums:
+        if num in inArray:
+            return True 
+        inArray.add(num)
+    return False
+
+print(containsDup([1, 2, 3, 1])) #Should be True
+print(containsDup([1,2,3,4])) #Should be false
+print(containsDup([1,1,1,3,3,4,3,2,4,2])) #Should be True
